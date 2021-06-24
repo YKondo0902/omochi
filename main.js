e424d57c871e68099caca94027451ec0724a0fc4
YKondo0902 <83995975+YKondo0902@users.noreply.github.com>
2021-06-25 06:16:55 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,34 @@
     overlay.classList.toggle('show');
     menu.classList.toggle('selected');
   });
-
   
+  /*リンク押したらスクロールさせる*/
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+  const header = document.querySelector('header');
+  
+  anchorLinks.forEach(link => {     
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const targetId = link.hash;
+      const targetElement = document.querySelector(targetId);
+      const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top;
+      const headerHeight = header.offsetHeight;
+      const totalScrollAmount = targetOffsetTop - headerHeight;
+
+      window.scrollTo({
+        top: totalScrollAmount,
+        behavior: "smooth"
+      });
+    });
+  });
+// トップに戻る
+  const home = document.querySelector('a[href="#TOP"]');
+home.addEventListener('click', e => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
 
 }
